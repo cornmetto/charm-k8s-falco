ef0040e4fe6ecd975d51e239bff7d408c84150b8
Cornellius Metto <corn.metto@canonical.com>
2021-07-09 17:05:13 +0300

    Run falco container using Pebble
    Use krisnova/falco-trace image to focus on userspace instrumentation
    for event sources.

--- src/charm.py
+++ src/charm.py
@@ -2,7 +2,6 @@
 # Copyright 2021 Cornellius Metto
 # See LICENSE file for licensing details.
 #
-# Learn more at: https://juju.is/docs/sdk
 
 import logging
 
@@ -17,33 +16,29 @@ logger = logging.getLogger(__name__)
 class CharmK8SFalcoCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
+        self.framework.observe(self.on.falco_pebble_ready, self._on_falco_pebble_ready)
 
-    def _on_httpbin_pebble_ready(self, event):
+    def _on_falco_pebble_ready(self, event):
         """
-        Define and start a workload using the Pebble API.
+        Set up the falco container.
         """
-        # Get a reference the container attribute on the PebbleReadyEvent
         container = event.workload
-        # Define an initial Pebble layer configuration
         pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
+            "summary": "falco layer",
+            "description": "pebble config layer for falco",
             "services": {
-                "httpbin": {
+                "falco": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
+                    "summary": "falco",
+                    "command": "/falco-trace/bin/falco",
                     "startup": "enabled",
                 }
             },
         }
-        # Add intial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
+
+        container.add_layer("falco", pebble_layer, combine=True)
         container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
+
         self.unit.status = ActiveStatus()
 
 
