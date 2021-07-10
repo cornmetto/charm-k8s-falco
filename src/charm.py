25f7612557d9f8b68bc8c5572bbadc956098b158
Cornellius Metto <corn.metto@canonical.com>
2021-07-10 09:42:15 +0300

    Configuration boilerplate

--- src/charm.py
+++ src/charm.py
@@ -6,17 +6,24 @@
 import logging
 
 from ops.charm import CharmBase
-from ops.framework import StoredState
+from ops.charm import ConfigChangedEvent
 from ops.main import main
 from ops.model import ActiveStatus
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 logger = logging.getLogger(__name__)
+
+TEMPLATES_DIR = "templates"
+FALCO_CONFIG_FILE = "/etc/falco/falco.yaml"
+template_env = Environment(loader=FileSystemLoader(
+    f"{TEMPLATES_DIR}/"), autoescape=select_autoescape())
 
 
 class CharmK8SFalcoCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.falco_pebble_ready, self._on_falco_pebble_ready)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
 
     def _on_falco_pebble_ready(self, event):
         """
@@ -40,6 +47,14 @@ class CharmK8SFalcoCharm(CharmBase):
         container.autostart()
 
         self.unit.status = ActiveStatus()
+
+    def _on_config_changed(self, event: ConfigChangedEvent):
+        """
+        Update falco's config.
+        """
+        template = template_env.get_template("falco.yaml")
+        container = self.unit.get_container("falco")
+        container.push(FALCO_CONFIG_FILE, template.render())
 
 
 if __name__ == "__main__":
